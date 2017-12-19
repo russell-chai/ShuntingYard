1ec2cf645edd1886a8a5876c055f047e3254a207
russell-chai <russell.chai@hotmail.com>
2017-12-19 11:23:03 -0800

    Create Student.h

---
+++ Student.h
@@ -0,0 +1,25 @@
+
+#ifndef STUDENT_H
+#define STUDENT_H
+
+#include <iostream>
+
+class Student {
+  public:
+    Student(char* newFirstName, char* newLastName, int newID, float newGPA);
+    ~Student();
+    void printInfo();
+    char* getFirstName();
+    char* getLastName();
+    int getID();
+    float getGPA();
+
+  private:
+    char* firstName;
+    char* lastName;
+    int ID;
+    float GPA;
+};
+
+#endif /*OBJECTS_H*/
+
