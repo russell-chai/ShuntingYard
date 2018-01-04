587d50744d561f95d80da8616dcfaef071865347
russell-chai <russell.chai@hotmail.com>
2018-01-04 11:00:45 -0800

    Update linkedList.cpp

--- linkedList.cpp
+++ linkedList.cpp
@@ -58,7 +58,6 @@ int main() {
     cout << "Type ADD to add student" << endl;
     cout << "Type PRINT to print this list of all students" << endl;
     cout << "Type DELETE to delete a student" << endl;
-    cout << "Type AVERAGE to average all the student's GPA" << endl;
     cout << "Type QUIT to quit program" << endl;
     char *input = new char(7);
     cin.getline(input, 7);
@@ -88,9 +87,6 @@ int main() {
       cout << "input student's ID" << endl;
       cin >> studentID;
       remove(head, head, studentID); 
-    }
-    if (strcmp(input, "AVERAGE") == 0) {
-      average(head, 0, 0);
     }
     if (strcmp(input, "QUIT") == 0) {
       return 0;
